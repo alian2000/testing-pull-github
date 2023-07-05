09bc93ed0f576ba0c3b598700ff470e17831e93e
alian2000 <engalian@gmail.com>
2023-07-05 12:30:47 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+def on_button_pressed_a():
+    basic.show_icon(IconNames.SMALL_SQUARE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_leds("""
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        """)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    basic.show_icon(IconNames.HEART)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    basic.show_number(input.temperature())
+    if input.temperature() > 31:
+        basic.show_string("hot")
+    else:
+        basic.show_string("cold")
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.showIcon(IconNames.Heart)
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    basic.showNumber(input.temperature())
+    if (input.temperature() > 31) {
+        basic.showString("hot")
+    } else {
+        basic.showString("cold")
+    }
+    
 })
